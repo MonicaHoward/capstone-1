a2eeafccd286dcd01611004ade5eff10ba5e4cd3
Monica Howard <mo@Monicas-Air.attlocal.net>
2021-02-09 11:30:02 -0600

    created Album component boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import './App.css';
+import Album from './components/Album'
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>PINKY'S RECORDS AND DISCS</h1>
+      <Album />
     </div>
   );
 }
---
+++ src/components/Album.jsx
@@ -0,0 +1,16 @@
+import React from 'react' 
+
+const Album = () => {
+
+    return(
+        <main>
+            <img src="https://place-hold.it/200x200/black" alt="album-cover"/>
+            <h2>Album Title</h2>
+            <h3>Artist Name</h3>
+            <p>$20</p>
+            <button>ADD TO CART</button>
+        </main>
+    )
+}
+
+export default Album
